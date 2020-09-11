cef32307f15ddf97f2f1b4fe7f4608ca4b569980
Bawn <lc5491137@gmail.com>
2020-09-11 17:56:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.2
 
 import PackageDescription
 
