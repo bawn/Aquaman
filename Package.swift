3bd25bd1f6f9ab210649ac3da7406799b49092dd
Lingchen <lingchen@tuya.com>
2020-09-12 10:16:44 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         .target(
             name: "Aquaman",
-            dependencies: [], path: "Aquaman/Aquaman")
+            path: "Aquaman/Aquaman")
     ]
 )
